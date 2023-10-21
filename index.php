3a11a2c674032a7de133c83551f7c7a4777e0523
James Lee <lee_mingjie@hotmail.com>
2023-10-22 00:00:10 +0800

    Some files

--- index.php
+++ index.php
@@ -6,12 +6,23 @@ $github = new Client();
 $github->authenticate('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', null, Client::AUTH_ACCESS_TOKEN);
 
 $repository = 'JamesLeeMingJie/github-php-api';
-$filePath = '';
+$filePath = 'docs/example.txt';
 $fileContent = file_get_contents($filePath);
 
-$result = $github->repos()->contents()->createFile(
+$result = $github->repos()->contents()->create(
+  'JamesLeeMingJie',
   $repository,
-  
-)
+  $filePath,
+  'Upload file from admin panel',
+  $fileContent
+);
+
+print_r($result);
+
+if ($result['content']['sha']) {
+  echo "Successful upload";
+} else {
+  echo "Upload fail";
+}
 
 ?>
